b96bbd985d58a7de5570ee82752259996bd57f44
saehoon0501 <saehoon0501@gmail.com>
2025-07-07 16:15:05 +0900

    tailwind added

--- claudia/src/main.jsx
+++ claudia/src/main.jsx
@@ -1,9 +1,14 @@
+import {StyledEngineProvider} from '@mui/material/styles';
+import GlobalStyles from '@mui/material/GlobalStyles';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <StyledEngineProvider enableCssLayer>
+      <GlobalStyles styles="@layer theme, base, mui, components, utilities;" />
+        <App />
+    </StyledEngineProvider>
   </React.StrictMode>
 ); 
