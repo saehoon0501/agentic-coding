4044e7cc2922229f4c4f5d756163247fc13cc10b
saehoon0501 <saehoon0501@gmail.com>
2025-07-07 14:58:26 +0900

    Initial commit: Add agentic coding test projects (claudia, codex, cursor, start_template)

---
+++ claudia/src/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TimeboxTimer from './timeboxTimer';
+
+function App() {
+  return (
+    <div style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', background: '#fafbfc' }}>
+      <TimeboxTimer />
+    </div>
+  );
+}
+
+export default App; 
